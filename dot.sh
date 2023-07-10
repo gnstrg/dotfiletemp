fdd1fd96ffd2ae816b58e876698b17453823077a
gnstrg <79290392+gnstrg@users.noreply.github.com>
2023-07-10 17:13:10 -0300

    Create dot.sh

---
+++ dot.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Baixar e instalar o Ransack
+wget -O ransack.deb "https://example.com/ransack.deb"  # Substitua o link pela URL correta
+sudo dpkg -i ransack.deb
+sudo apt-get install -f -y
+
+# Baixar e instalar o Chrome
+wget -O chrome.deb "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+sudo dpkg -i chrome.deb
+sudo apt-get install -f -y
+
+# Baixar e instalar o Telegram
+sudo snap install telegram-desktop
+
+# Remover arquivos .deb após a instalação
+rm ransack.deb chrome.deb
+
